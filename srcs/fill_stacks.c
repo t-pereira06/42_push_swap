46accaa000d14329ff31f40f594dca898d40e1ea
tsodre-p <tsodre-p@student.42porto.com>
2023-01-19 15:13:33 +0000

    starting fill_stacks

--- srcs/fill_stacks.c
+++ srcs/fill_stacks.c
@@ -1,26 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sb.c                                               :+:      :+:    :+:   */
+/*   fill_stacks.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/19 11:29:36 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/01/19 15:11:34 by tsodre-p         ###   ########.fr       */
+/*   Created: 2023/01/19 15:06:32 by tsodre-p          #+#    #+#             */
+/*   Updated: 2023/01/19 15:12:55 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/push_swap.h"
-
-/*swap the first element with the second in stack b*/
-void	sb(t_stack *stack_b)
+/*functions to fill the stacks, counting the size of it and fill it in the struct field size*/
+void	fill_stacks(int argc, char *argv)
 {
-	int tmp;
 
-	if (stack_b->size > 1)
-	{
-		tmp = stack_b->array[0];
-		stack_b->array[0] = stack_b->array[1];
-		stack_b->array[1] = tmp;
-	}
 }
--- srcs/sa.c
+++ srcs/sa.c
@@ -6,12 +6,13 @@
 /*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 11:04:51 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/01/19 14:50:04 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2023/01/19 15:08:24 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
+/*swap the first element with the second in stack a*/
 void	sa(t_stack *stack_a)
 {
 	int tmp;
